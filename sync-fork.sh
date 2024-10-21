5dc254985df0c9e1adb7145f4176de34141669bc
Cside <cside.story@gmail.com>
2024-10-21 16:07:20 +0900

    update

--- sync-fork.sh
+++ sync-fork.sh
@@ -4,15 +4,15 @@ set -eux
 date=$(date +%Y-%m-%d)
 branch="fix-$date"
 
-# if ! git remote -v | grep upstream >/dev/null; then
-#   git remote add upstream git@github.com:crxjs/chrome-extension-tools.git
-# fi
-# 
-# git checkout main
-# git checkout -b "$branch"
-# 
-# git fetch upstream
-# git merge upstream/main
+if ! git remote -v | grep upstream >/dev/null; then
+  git remote add upstream git@github.com:crxjs/chrome-extension-tools.git
+fi
+
+git checkout main
+git checkout -b "$branch"
+
+git fetch upstream
+git merge upstream/main
 
 pnpm install
 pnpm build:vite-plugin
